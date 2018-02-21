66ac12da98ea61825ac44ab6b85034b11d35577d
Cathy Tang <cathy.tang11@gmail.com>
2018-02-20 20:31:50 -0500

    initial commit

---
+++ dev/scripts/main.js
@@ -0,0 +1,6 @@
+const ohHey = "Hello World";
+
+console.log(ohHey);
+
+console.log(`hello`)
+//only make changes in dev folders, not public folders!
---
+++ public/scripts/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var ohHey = "Hello World";
+
+console.log(ohHey);
+
+console.log("hello");
+//only make changes in dev folders, not public folders!
